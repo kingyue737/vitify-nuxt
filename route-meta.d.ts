c6c1e6270b2440f2787214b521681bd6e7bc4348
Yue JIN <yjin@nustarnuclear.com>
2023-10-27 17:57:24 +0800

    refactor: remove unused route meta declaration

--- route-meta.d.ts
+++ route-meta.d.ts
@@ -1,15 +1,5 @@
 export {}
 
-import 'vue-router'
-
-declare module 'vue-router' {
-  interface RouteMeta {
-    icon?: string
-    title?: string
-    drawerIndex?: number
-  }
-}
-
 declare module '#app' {
   interface PageMeta {
     icon?: string
