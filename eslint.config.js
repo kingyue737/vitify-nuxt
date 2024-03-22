529615e22742fdd9a65a00538744e7b3012cca0a
Yue JIN <yjin@nustarnuclear.com>
2024-03-22 10:07:53 +0800

    chore: up @nuxt/eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,6 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 export default withNuxt({
   rules: {
     'vue/valid-v-slot': 'off',
-    'vue/html-closing-bracket-newline': 'off',
-    'vue/singleline-html-element-content-newline': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
 })
