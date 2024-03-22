56fa246313dc17903c0706ac66867af4c1c3cfe3
Yue JIN <yjin@nustarnuclear.com>
2024-03-22 15:03:31 +0800

    chore: modify vue/valid-v-slot lint rule

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt({
   rules: {
-    'vue/valid-v-slot': 'off',
+    'vue/valid-v-slot': ['error', { allowModifiers: true }],
     '@typescript-eslint/no-explicit-any': 'off',
   },
 })
