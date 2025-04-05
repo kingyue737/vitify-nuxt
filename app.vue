c619eae91437a8046826875efc7c4671d7181c9e
Yue JIN <yuejin13@qq.com>
2025-04-05 19:17:24 +0800

    feat: up to vuetify 3.8.0 and nuxt-echarts 0.3.0

--- app.vue
+++ app.vue
@@ -39,9 +39,13 @@ useSeoMeta({
 .v-main {
   padding-top: 0;
   padding-bottom: 0;
-  margin-top: var(--v-layout-top);
+  /* https://github.com/vuetifyjs/vuetify/issues/15202 */
+  margin-top: 64px;
+  margin-bottom: 32px;
+  height: calc(100vh - 64px - 32px);
+  /* margin-top: var(--v-layout-top);
   margin-bottom: var(--v-layout-bottom);
-  height: calc(100vh - var(--v-layout-top) - var(--v-layout-bottom));
+  height: calc(100vh - var(--v-layout-top) - var(--v-layout-bottom)); */
   overflow-y: auto;
   transition-property: padding;
 }
