ba66e38567211559abf83852a811f3b6bddb3165
Yue JIN <yuejin13@qq.com>
2025-07-09 13:20:34 +0800

    chore: explicitly add defineNuxtConfig to bypass test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 import { aliases } from 'vuetify/iconsets/mdi'
+import { defineNuxtConfig } from 'nuxt/config'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
